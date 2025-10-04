ced8cc7c9cff366d948c5c05538251c15fec3775
bniladridas <bniladridas@users.noreply.github.com>
2025-10-04 17:12:16 +0530

    replace bash e2e script with python to avoid ps parsing

---
+++ scripts/e2e.py
@@ -0,0 +1,51 @@
+import os
+import subprocess
+import sys
+
+runner_os = os.environ.get('RUNNER_OS', '')
+
+if runner_os == 'Windows':
+    # Activate conda
+    subprocess.run(['source', '/c/Miniconda/etc/profile.d/conda.sh'], shell=True)
+    subprocess.run(['conda', 'activate', 'base'], shell=True)
+
+# Create dirs
+os.makedirs('test_images/tiles', exist_ok=True)
+os.makedirs('test_images/upscaled', exist_ok=True)
+
+# Create test image
+subprocess.run([sys.executable, 'create_test_image.py'])
+
+# Preprocess
+if runner_os == 'Windows':
+    subprocess.run(['./build/Release/preprocess.exe', 'test_images', 'test_images/tiles'])
+else:
+    subprocess.run(['./build/preprocess', 'test_images', 'test_images/tiles'])
+
+print("Preprocess done")
+
+# Copy tiles
+subprocess.run(['cp', 'test_images/tiles/*', 'test_images/upscaled/'], shell=True)
+print("Cp done")
+
+# Rename tiles
+for i in range(16):
+    src = f'test_images/upscaled/test_tile_{i}.jpg'
+    dst = f'test_images/upscaled/tile_{i}.jpg'
+    if os.path.exists(src):
+        os.rename(src, dst)
+print("Mv done")
+
+# Stitch
+if runner_os == 'Windows':
+    subprocess.run([sys.executable, 'scripts/stitch.py'])
+else:
+    subprocess.run([sys.executable, '-m', 'coverage', 'run', '--source=scripts', 'scripts/stitch.py'])
+
+print("Stitch done")
+
+# Verify
+if os.path.exists('test_images/final_output.jpg'):
+    print("E2E test passed")
+else:
+    sys.exit(1)
