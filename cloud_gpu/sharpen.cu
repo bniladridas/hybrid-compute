4cd9a93fd65951442567ab7a465cc08c728d28fb
Niladri Das <bniladridas@users.noreply.github.com>
2025-10-24 03:44:30 +0530

    style: format cuda files with clang-format

--- cloud_gpu/sharpen.cu
+++ cloud_gpu/sharpen.cu
@@ -3,20 +3,21 @@
  */
 
 // Placeholder
-__global__ void sharpenKernel(uchar* input, uchar* output, int width, int height) {
-    // TODO
+__global__ void sharpenKernel(uchar *input, uchar *output, int width,
+                              int height) {
+  // TODO
 }
 
-int applySharpening(const cv::Mat& input, cv::Mat& output) {
-    output = input.clone();
-    return 0;
+int applySharpening(const cv::Mat &input, cv::Mat &output) {
+  output = input.clone();
+  return 0;
 }
 
-int main(int argc, char** argv) {
-    cv::Mat input = cv::imread(argv[1]);
-    cv::Mat output;
-    applySharpening(input, output);
-    cv::imwrite(argv[2], output);
-    std::cout << "Sharpening applied." << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+  cv::Mat input = cv::imread(argv[1]);
+  cv::Mat output;
+  applySharpening(input, output);
+  cv::imwrite(argv[2], output);
+  std::cout << "Sharpening applied." << std::endl;
+  return 0;
 }
--- cloud_gpu/threshold.cu
+++ cloud_gpu/threshold.cu
@@ -3,21 +3,22 @@
  */
 
 // Placeholder
-__global__ void thresholdKernel(uchar* input, uchar* output, int width, int height, int thresh) {
-    // TODO
+__global__ void thresholdKernel(uchar *input, uchar *output, int width,
+                                int height, int thresh) {
+  // TODO
 }
 
-int applyThresholding(const cv::Mat& input, cv::Mat& output, int thresh) {
-    output = input.clone();
-    return 0;
+int applyThresholding(const cv::Mat &input, cv::Mat &output, int thresh) {
+  output = input.clone();
+  return 0;
 }
 
-int main(int argc, char** argv) {
-    cv::Mat input = cv::imread(argv[1]);
-    cv::Mat output;
-    int thresh = atoi(argv[3]);
-    applyThresholding(input, output, thresh);
-    cv::imwrite(argv[2], output);
-    std::cout << "Thresholding applied." << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+  cv::Mat input = cv::imread(argv[1]);
+  cv::Mat output;
+  int thresh = atoi(argv[3]);
+  applyThresholding(input, output, thresh);
+  cv::imwrite(argv[2], output);
+  std::cout << "Thresholding applied." << std::endl;
+  return 0;
 }
