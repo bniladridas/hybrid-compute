8b76a4f36b58b64b7a263927c6c052a1e2fd4d1c
Niladri Das <bniladridas@users.noreply.github.com>
2025-10-24 03:44:30 +0530

    style: format cuda files with clang-format

--- cloud_gpu/canny.cu
+++ cloud_gpu/canny.cu
@@ -3,20 +3,21 @@
  */
 
 // Placeholder
-__global__ void cannyKernel(uchar* input, uchar* output, int width, int height) {
-    // TODO
+__global__ void cannyKernel(uchar *input, uchar *output, int width,
+                            int height) {
+  // TODO
 }
 
-int applyCanny(const cv::Mat& input, cv::Mat& output) {
-    output = input.clone();
-    return 0;
+int applyCanny(const cv::Mat &input, cv::Mat &output) {
+  output = input.clone();
+  return 0;
 }
 
-int main(int argc, char** argv) {
-    cv::Mat input = cv::imread(argv[1]);
-    cv::Mat output;
-    applyCanny(input, output);
-    cv::imwrite(argv[2], output);
-    std::cout << "Canny applied." << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+  cv::Mat input = cv::imread(argv[1]);
+  cv::Mat output;
+  applyCanny(input, output);
+  cv::imwrite(argv[2], output);
+  std::cout << "Canny applied." << std::endl;
+  return 0;
 }
