d282f498c365cde6071fd5ba8fdafc8c02b0c20a
Niladri Das <bniladridas@users.noreply.github.com>
2025-10-24 10:49:25 +0530

    feat: granular benchmark control via benchmark.cmake

---
+++ benchmark.cmake
@@ -0,0 +1,4 @@
+# Benchmark control options
+option(ENABLE_BENCHMARK "Enable Google Benchmark library and tests" ON)
+option(ENABLE_BENCHMARK_TESTING "Enable Google Benchmark internal tests" OFF)
+option(ENABLE_BENCHMARK_GTEST_TESTS "Enable Google Benchmark GTest tests" OFF)
