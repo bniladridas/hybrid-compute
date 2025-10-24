8c2ddb6997b14bc2f19c486da81e0d665f3ffb38
Niladri Das <bniladridas@users.noreply.github.com>
2025-10-24 06:26:50 +0530

    feat: update build scripts and cmake for better performance

--- scripts/transfer_tiles.sh
+++ scripts/transfer_tiles.sh
@@ -8,7 +8,7 @@ CLOUD_PROJECT_PATH="/home/ubuntu/HybridCompute"
 scp -r ./test_images/tiles $CLOUD_USER@$CLOUD_IP:$CLOUD_PROJECT_PATH/tiles
 
 # Run CUDA upscaler on cloud
-ssh $CLOUD_USER@$CLOUD_IP "cd $CLOUD_PROJECT_PATH && nvcc cloud_gpu/upscale.cu -o upscaler -I/usr/local/include/opencv4 -L/usr/local/lib -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -std=c++11"
+ssh $CLOUD_USER@$CLOUD_IP "cd $CLOUD_PROJECT_PATH && nvcc cloud_gpu/upscale.cu -o upscaler -I/usr/local/include/opencv4 -L/usr/local/lib -lopencv_core -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -std=c++17"
 
 # Transfer results back
 scp -r $CLOUD_USER@$CLOUD_IP:$CLOUD_PROJECT_PATH/upscaled ./test_images/
