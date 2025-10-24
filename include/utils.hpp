5c35d02aefd5e34753b354ce4d90acfd103dfb4a
Niladri Das <bniladridas@users.noreply.github.com>
2025-10-24 11:38:35 +0530

    style: format header files in include/ with clang-format
    - apply llvm style formatting to .h and .hpp files

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,13 +2,13 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-std::vector<cv::Mat> splitImageIntoTiles(const cv::Mat& image, int tile_size) {
-    std::vector<cv::Mat> tiles;
-    for (int y = 0; y < image.rows; y += tile_size) {
-        for (int x = 0; x < image.cols; x += tile_size) {
-            cv::Rect roi(x, y, tile_size, tile_size);
-            tiles.push_back(image(roi).clone());
-        }
+std::vector<cv::Mat> splitImageIntoTiles(const cv::Mat &image, int tile_size) {
+  std::vector<cv::Mat> tiles;
+  for (int y = 0; y < image.rows; y += tile_size) {
+    for (int x = 0; x < image.cols; x += tile_size) {
+      cv::Rect roi(x, y, tile_size, tile_size);
+      tiles.push_back(image(roi).clone());
     }
-    return tiles;
+  }
+  return tiles;
 }
