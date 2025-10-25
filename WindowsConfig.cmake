e9738a2d2b443f3bc68927003b0262d8912b5e43
Niladri Das <bniladridas@users.noreply.github.com>
2025-10-26 04:59:16 +0530

    fix: static msvc runtime on windows

--- WindowsConfig.cmake
+++ WindowsConfig.cmake
@@ -3,3 +3,6 @@
 set(ENABLE_BENCHMARK
     ON
     CACHE BOOL "Enable benchmarks on Windows" FORCE)
+
+# Set MSVC runtime to static to avoid linking conflicts with benchmark and gtest
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
