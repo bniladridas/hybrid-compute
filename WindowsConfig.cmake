9ee41ab5d76c018a5bebd6da016830e76e2dfca1
Niladri Das <bniladridas@users.noreply.github.com>
2025-10-26 05:31:47 +0530

    fix: set dynamic runtime in windowsconfig.cmake

--- WindowsConfig.cmake
+++ WindowsConfig.cmake
@@ -4,5 +4,5 @@ set(ENABLE_BENCHMARK
     ON
     CACHE BOOL "Enable benchmarks on Windows" FORCE)
 
-# Set MSVC runtime to static to avoid linking conflicts with benchmark and gtest
-set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
+# Set MSVC runtime to dynamic to avoid linking conflicts with benchmark and gtest
+set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreadedDLL")
