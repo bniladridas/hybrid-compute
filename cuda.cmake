092f5c4fffa1f6b65c53cc309b8de110a0924f05
Niladri Das <bniladridas@users.noreply.github.com>
2025-10-28 08:01:42 +0530

    ci: support multiple cuda architectures

--- cuda.cmake
+++ cuda.cmake
@@ -1,3 +1,3 @@
 # CUDA control options
 option(USE_CUDA "Build with CUDA support" OFF)
-option(CUDA_ARCH "CUDA architecture to target" "75")
+option(CUDA_ARCH "CUDA architecture to target" "75;80;86")
